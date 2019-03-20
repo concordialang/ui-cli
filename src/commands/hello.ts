8e651c579547f3660d342f0782f5df7350e71d22
Willian <w.goncalves91@gmail.com>
2019-03-20 16:19:53 -0300

    Send the provided file path to the core

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,31 +1,34 @@
 import {Command, flags} from '@oclif/command'
+//import * as core from '/home/willian/Projects/tcc/ui-core'
+import {AstProcessor} from '/home/willian/Projects/tcc/ui-core'
+
+/*
+ * Run "npm run build" in the ui-core folder.
+ * Then, set the right path to AstProcessor.
+ * Run the command "cli hello --path /path/to/anyFile.json"
+ */
 
 export default class Hello extends Command {
   static description = 'describe the command here'
 
-  static examples = [
-    `$ concordialang-ui-cli hello
-hello world from ./src/hello.ts!
-`,
-  ]
-
   static flags = {
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    path: flags.string({char: 'p', description: 'ast file path'}),
   }
 
-  static args = [{name: 'file'}]
+  //static args = [{name: 'file'}]
 
   async run() {
-    const {args, flags} = this.parse(Hello)
-
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from .\\src\\commands\\hello.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    const {flags} = this.parse(Hello)
+    const processor = new AstProcessor()
+    if (flags.path) {
+      const result = await processor.process(flags.path)
+      this.log(JSON.stringify(result))
     }
+
+    //const name = flags.name || 'world'
+    //this.log(`hello ${name} from .\\src\\commands\\hello.ts`)
+    //if (args.file && flags.force) {
+      //this.log(`you input --force and --file: ${args.file}`)
+    //}
   }
 }
